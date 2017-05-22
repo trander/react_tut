edaf043d6d047bb475fdba48ebaa5722e79e8e77
Hyunjung Lee <trlee329@gmail.com>
2017-05-22 14:46:41 +0200

    App -> root element : rendering

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import React from 'react-dom';
+import App from './components/App';
+
+const reetElement = document.getElementById('root');
+ReactDOM.render(
+    <App />,
+    rootElement
+);
