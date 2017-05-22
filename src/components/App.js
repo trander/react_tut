af9a0825287f0ce5558071acbc083ba903bd7a18
hyunjung lee <hyunjung lee>
2017-05-22 16:57:02 +0200

    1. js 2. method

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
-
+    sayHey() {
+        alert("hey");
+    }
 
     render(){
-
+        let text="Dev-Server"
         return (
             <div>
                 <h1>Hello Trander</h1>
-                
+                <h2>Welcome to {text}</h2>
+                <button onClick={this.sayHey}>Click Me</button>
             </div>
         );
     }
