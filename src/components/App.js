76f1743392287cbc7e72bf8b32fbd87b85622303
hyunjung lee <hyunjung lee>
2017-05-23 11:16:13 +0200

    porps practice

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,9 @@ class App extends React.Component {
     render(){
         return  (
             <div>
-                <Header/>
-                <Content/>
+                <Header title={ this.props.headerTitle }/>
+                <Content title={ this.props.contentTitle }
+                        body={ this.props.contentBody }/>
             </div>
         );
     }
