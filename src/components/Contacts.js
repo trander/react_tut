710cc5e6d5f99a9c58c40787492894d647afcb3a
hyunjung lee <hyunjung lee>
2017-05-23 12:14:19 +0200

    Contacts practice

---
+++ src/components/Contacts.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Contacts extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Contacts </h1>
+                <ul>
+                    <li> A 010-0000-0001 </li>
+                    <li> B 010-0000-0002 </li>
+                    <li> C 010-0000-0003 </li>
+                    <li> D 010-0000-0004 </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Contacts;
