e395a18eb5af54ceb16888e806bd306ee0710301
hyunjung lee <hyunjung lee>
2017-05-23 11:31:46 +0200

    state practice

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
 import Content from './Content';
+import StateExample from './StateExample';
 
 class App extends React.Component {
     render(){
@@ -10,6 +11,7 @@ class App extends React.Component {
                 <Header title={ this.props.headerTitle }/>
                 <Content title={ this.props.contentTitle }
                         body={ this.props.contentBody }/>
+                <StateExample />
             </div>
         );
     }
@@ -17,8 +19,8 @@ class App extends React.Component {
 
 App.defaultProps = {
     headerTitle: 'Default Header',
-    contentTitle: 5,
-    contentBody: undefined
+    contentTitle: 'Default Content Title',
+    contentBody: 'Default Content Body'
 };
 
 export default App;
--- src/components/StateExample.js
+++ src/components/StateExample.js
@@ -3,28 +3,58 @@ import React from 'react';
 class StateExample extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             header: "Header Initial State",
             content: "Content Initial State"
         };
     }
 
-    updateHeader(text){
+    updateHeader() {
         this.setState({
             header: "Header has changed"
         });
     }
 
     render() {
-        return (
+        return(
             <div>
-                <h1> { this.state.header } </h1>
-                <h2> { this.state.content }</h2>
-                <button onClick={this.updateHeader.bind(this)}>Update</button>
+                <h1>{ this.state.header }</h1>
+                <h2>{ this.state.content }</h2>
+                <button onClick={ this.updateHeader.bind(this) }>Update</button>
             </div>
         );
     }
 }
 
 export default StateExample;
+
+// import React from 'react';
+//
+// class StateExample extends React.Component {
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             header: "Header Initial State",
+//             content: "Content Initial State"
+//         };
+//     }
+//
+//     updateHeader(text){
+//         this.setState({
+//             header: "Header has changed"
+//         });
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <h1> { this.state.header } </h1>
+//                 <h2> { this.state.content }</h2>
+//                 <button onClick={this.updateHeader.bind(this)}>Update</button>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default StateExample;
