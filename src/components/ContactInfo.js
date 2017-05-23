0543e0cbcc30a59b0d04859fbe0f95cdf8e0a0f1
hyunjung lee <hyunjung lee>
2017-05-23 16:02:16 +0200

    +handleClick( )

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 class ContactInfo extends React.Component {
+    handleClick(){
+        this.props.onSelect(this.props.contactKey);
+    }
+
     render() {
         return(
-            <li>{ this.props.name } {this.props.phone}</li>
+            <li onClick={this.handleClick.bind(this)}>{ this.props.name } {this.props.phone}</li>
         );
     }
 }
