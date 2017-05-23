5c78dd897fcc4954898d379c1f49466ebcf8e86e
hyunjung lee <hyunjung lee>
2017-05-23 11:56:10 +0200

    state and props practice

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import Header from './Header';
 import Content from './Content';
 import StateExample from './StateExample';
+import RandomNumber from './RandomNumber';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: Math.round(Math.random()*100)
+        };
+
+        this.updateValue = this.updateValue.bind(this);
+    }
+
+    updateValue(randomValue) {
+        this.setState({
+            value:randomValue
+        });
+    }
+
+
     render(){
         return  (
             <div>
@@ -12,6 +29,8 @@ class App extends React.Component {
                 <Content title={ this.props.contentTitle }
                         body={ this.props.contentBody }/>
                 <StateExample />
+                <RandomNumber number={ this.state.value }
+                                onUpdate={this.updateValue }/>
             </div>
         );
     }
--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 class RandomNumber extends React.Component {
-    updateNumber(){
-        let value = Math.round(Math.random()*100);
+    updateNumber() {
+        let value=Math.round(Math.random()*100);
         this.props.onUpdate(value);
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.updateNumber = this.updateNumber.bind(this);
     }
 
-    render(){
+    render() {
         return (
             <div>
-                <h1>RANDOM NUMBER: { this.props.number }</h1>
-                <button onClick={this.updateNumber}>Randomize</button>
+                <h1>RANDOM NUMBER : { this.props.number }</h1>
+                <button onClick={ this.updateNumber }> Random </button>
             </div>
         );
     }
 }
 
 export default RandomNumber;
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+//
+// class RandomNumber extends React.Component {
+//     updateNumber(){
+//         let value = Math.round(Math.random()*100);
+//         this.props.onUpdate(value);
+//     }
+//
+//     constructor(props){
+//         super(props);
+//         this.updateNumber = this.updateNumber.bind(this);
+//     }
+//
+//     render(){
+//         return (
+//             <div>
+//                 <h1>RANDOM NUMBER: { this.props.number }</h1>
+//                 <button onClick={this.updateNumber}>Randomize</button>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default RandomNumber;
